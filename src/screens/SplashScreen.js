1573a71676d60c1c4866de75f3648db64ade8849
Erkezhan Tleukulova <erkiame3@gmail.com>
2023-02-17 17:29:11 +0600

    logo

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,9 +1,10 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Image} from 'react-native';
 import React, {useEffect} from 'react';
 import {GlobalStyles} from '../constants/globalStyles';
 import {useDispatch, useSelector} from 'react-redux';
 import {userGetInfo} from '../store/reducers/userReducer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import logo from './../assets/img/logo.jpg';
 
 const SplashScreen = ({navigation}) => {
   const dispatch = useDispatch();
@@ -15,8 +16,17 @@ const SplashScreen = ({navigation}) => {
   }, []);
 
   return (
-    <SafeAreaView style={[GlobalStyles.container]}>
-      <Text>Chess point</Text>
+    <SafeAreaView
+      style={[
+        GlobalStyles.container,
+        GlobalStyles.flexJustifyBetween,
+        GlobalStyles.flexAlignCenter,
+      ]}>
+      <Image
+        style={{width: '100%', height: 200}}
+        resizeMode="contain"
+        source={logo}
+      />
     </SafeAreaView>
   );
 };
